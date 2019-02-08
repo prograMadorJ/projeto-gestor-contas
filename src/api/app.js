7b185f98a50bd072f58b082e6f6195b971dba541
projetocvjobs <projetocvjobs@gmail.com>
2019-02-08 21:15:45 -0200

    custom errors 404 and 500

--- src/api/app.js
+++ src/api/app.js
@@ -4,11 +4,27 @@ const DB = require('./data/config')
 
 DB.connect('localhost','27017','contas')
 
+app.enabled('trust proxy')
+
+const serverErrorHandler = (err, req, res, next) => {
+    res.status(500).send('Algo de errado aconteceu, tente novamente')
+    console.log(err)
+
+}
+
+const clientErrorHandler = (req, res) => {
+    res.status(404).send('Recurso ou serviço não disponível no momento')
+}
+
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 
 require('./routes/Routes')(app)
+
+app.use(serverErrorHandler)
+app.use(clientErrorHandler)
 
 module.exports = app
