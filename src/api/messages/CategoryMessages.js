c9eb3de604b792dd7682f318c4a6abfc99eb147b
projetocvjobs <projetocvjobs@gmail.com>
2019-02-09 00:27:29 -0200

    implementations category

---
+++ src/api/messages/CategoryMessages.js
@@ -0,0 +1,5 @@
+module.exports = {
+    REGISTERED_SUCCESS: 'Categoria cadastrada com sucesso!',
+    UPDATED_SUCCESS: 'Categoria atualizada com sucesso!',
+    DELETED_SUCCESS: 'Categoria deletada com sucesso!'
+}
---
+++ src/api/routes/CategoryRoute.js
@@ -0,0 +1,4 @@
+const controller = require('../controllers/CategoryController')
+const Base = require('./BaseRoute')
+
+module.exports = Base('/api/category',controller)
