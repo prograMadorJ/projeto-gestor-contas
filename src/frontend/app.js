3fdea04785d903e21f63076a6064e35b37fce56b
prograMadorJ <jorge.fernando.brito@gmail.com>
2019-02-16 15:31:40 -0200

    added body parser

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,5 +1,6 @@
 const es6Engine = require('express-es6-template-engine')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 module.exports = (app) => {
 
@@ -13,10 +14,15 @@ module.exports = (app) => {
         res.status(404).send('view 404')
     }
 
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }))
     app.engine('html', es6Engine)
     app.set('views', 'src/frontend/views')
     app.set('view engine', 'html')
     app.use(express.static(__dirname + '/public'))
+
 
     require('./routes/Routes')(app)
 
