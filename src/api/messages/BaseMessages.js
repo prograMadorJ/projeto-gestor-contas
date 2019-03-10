a9acb8fd6c9bccca0530380c4336e17307a610a7
prograMadorJ <jorge.fernando.brito@gmail.com>
2019-03-10 14:39:31 -0300

    implementation new messages

--- src/api/messages/BaseMessages.js
+++ src/api/messages/BaseMessages.js
@@ -1,4 +1,8 @@
 module.exports = {
     SERVER_ERROR: 'Algo de errado aconteceu, tente novamente',
-    RESOURCE_NOT_FOUND: 'Recurso ou serviço não disponível no momento'
+    RESOURCE_NOT_FOUND: 'Recurso ou serviço não disponível no momento',
+    REGISTER_INVALID: 'Parâmentros para efetuar registro são inválidos',
+    UPDATE_INVALID: 'Parâmetros para efetuar atulização de cadastro são inválidos',
+    GET_INVALID: 'Parâmetros para efeturar busca de um registro são inválidos',
+    DELETE_INVALID: 'Parâmetros para deletar registro são inválidos'
 }
