21e03ace98ebae497a39376bd33b447b048655c9
prograMadorJ <jorge.fernando.brito@gmail.com>
2019-03-11 01:38:10 -0300

    added new message

--- src/api/messages/ExpenseMessages.js
+++ src/api/messages/ExpenseMessages.js
@@ -1,5 +1,6 @@
 module.exports = {
     REGISTERED_SUCCESS: 'Despesa cadastrada com sucesso!',
     UPDATED_SUCCESS: 'Despesa atualizada com sucesso!',
-    DELETED_SUCCESS: 'Despesa deletada com sucesso!'
+    DELETED_SUCCESS: 'Despesa deletada com sucesso!',
+    REGISTER_NOT_FOUND: 'Nenhuma despesa foi encontrada.'
 }
