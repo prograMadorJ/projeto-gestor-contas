8b1c1a1b3842d572bf4196d6031effcd8df67e52
prograMadorJ <jorge.fernando.brito@gmail.com>
2019-03-11 10:04:04 -0300

    added new message

--- src/api/messages/CategoryMessages.js
+++ src/api/messages/CategoryMessages.js
@@ -1,5 +1,6 @@
 module.exports = {
     REGISTERED_SUCCESS: 'Categoria cadastrada com sucesso!',
     UPDATED_SUCCESS: 'Categoria atualizada com sucesso!',
-    DELETED_SUCCESS: 'Categoria deletada com sucesso!'
+    DELETED_SUCCESS: 'Categoria deletada com sucesso!',
+    REGISTER_NOT_FOUND: 'Nenhuma categoria foi encontrada.'
 }
